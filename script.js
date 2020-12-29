1469b18aab7b56771655cdab14805987dc774955
Jari Helsloot <70521824+Jarih1995@users.noreply.github.com>
2020-12-29 13:53:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-// Opdracht A
-const ikRockArrowFunctions = () => {
-  console.log("Joe, ik rock de arrow functions!");
-};
-
-ikRockArrowFunctions();
-
-// Opdracht B
-const fivePlusSeven = () => 5 + 7;
-
-fivePlusSeven();
-
-// Opdracht C
-const myFunction = (a, b) => a + b;
-
-// Opdracht D
-const addFive = a => a + 5;
-
-// Opdracht E
-const createObject = () => ({ value: "hoi" });
+const array = [
+    { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+    { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
+    { name: "A. Curry", profession: "kikvorsman", age: 32 },
+    { name: "F. Vonk", profession: "slangenmelker", age: 36 },
+    { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
+    { name: "Dr.Evil", profession: "digital overlord", age: 56 }
+  ];
+  
+  for (let person of array) {
+    // console.log("Dit is de gehele persoon:", person);
+    // console.log ("De naam is", person.name);
+    // console.log("Het geboorte jaar is", 2020 - person.age)
+    // console.log(person.name, 'is', person.profession) 
+    if person.age == 50 {
+        console.log(person.name,'is ouder dan 50')
+    }}
