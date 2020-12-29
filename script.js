0d2c128ed682f218d2e05d07674918718c3cd0e6
jarih1995 <j.helsloot@live.nl>
2020-12-29 13:01:05 +0100

    MODULE 5 OPDRACHT 1

---
+++ script.js
@@ -0,0 +1,10 @@
+// // zet de functie om naar een arrow functie
+// const ikRockArrowFunctions = () => console.log("Jari, ik rock de arrow functions!");
+  
+//   ikRockArrowFunctions()
+
+  const fivePlusSeven = function () {
+    return 5 + 7
+  };
+  
+  console.log(fivePlusSeven)
