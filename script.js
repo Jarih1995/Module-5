9e2b2b62422cd3fcd356e88959ad12590177246e
Jari Helsloot <70521824+Jarih1995@users.noreply.github.com>
2020-12-29 14:56:36 +0100

    Module 5 - opdracht 3

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
-const array = [
-    { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
-    { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
-    { name: "A. Curry", profession: "kikvorsman", age: 32 },
-    { name: "F. Vonk", profession: "slangenmelker", age: 36 },
-    { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
-    { name: "Dr.Evil", profession: "digital overlord", age: 56 }
-  ];
-  
-  for (let person of array) {
-    // console.log("Dit is de gehele persoon:", person);
-    // console.log ("De naam is", person.name);
-    // console.log("Het geboorte jaar is", 2020 - person.age)
-    // console.log(person.name, 'is', person.profession) 
-    if person.age == 50 {
-        console.log(person.name,'is ouder dan 50')
-    }}
+// a ?
+
+// const addTheWordCool = function(array){
+//     array.push('cool')
+// }
+// console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+// // resultaat: ["nice", "awesome", "tof", "cool"]
+
+
+// B
+
+// const amountOfElementsInArray  = (array)  =>  console.log(array.length);
+
+
+// console.log(amountOfElementsInArray(['appels', 'peren', 'citroenen'])); 
+
+
+// // C 
+
+// const selectBelgiumFromBenelux = (arr) => console.log(arr[0])
+
+// console.log(selectBelgiumFromBenelux(["Belgie", "Nederland", "Luxemburg"])); 
+
+
+// // D
+
+
+// const lastElementInArray = (arr) => console.log(arr[arr.length - 1])
+// console.log(lastElementInArray(["Haas", "Cavia", "Kip", "Schildpad"])); 
+
+
+// E ?
+
+// const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+
+// // const impeachTrumpSlice = function(array) {
+// // presidents.slice(pos, 0)
+// // }
+// const impeachTrumpSplice = function(array) {
+//      presidents.splice(pos, 0)
+// }
+
+// // console.log(impeachTrumpSlice(presidents)); // ["Obama", "Bush", "Clinton"]
+// console.log(impeachTrumpSplice(presidents)); // ["Obama", "Bush", "Clinton"]
+
+// F ?
+
+// const stringsTogether = function(arr) {
+
+//     arr.prototype.join()
+// }
+
+// console.log(stringsTogether(['Winc', 'Academy', 'is', 'leuk', ';-}'])) 
+// //resultaat: "Winc Academy is leuk ;-}"
+
+
+// G  ? 
+
+// const combineArrays = ([1,2,3],[4,5,6]) => 
+
